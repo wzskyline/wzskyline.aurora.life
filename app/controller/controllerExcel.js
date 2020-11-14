92bce7b697174a30ce197ec0028fef4f004357c7
wzskyline <wzskyline@qq.com>
2020-11-14 09:27:39 +0800

    'add'

--- app/controller/controllerExcel.js
+++ app/controller/controllerExcel.js
@@ -1,16 +1,17 @@
 'use strict';
 
 const Controller = require('egg').Controller;
+const table
 function col(arr){
  return {
-   number:arr[0] || '',
-   desc:arr[1] || '',
-   type:arr[2] || '',
-   type:arr[3] || '',
-   type:arr[4] || '',
-   type:arr[5] || '',
-   type:arr[6] || '',
-   date:arr[7] || '',
+  number:arr[0] || '',
+  name:arr[1] || '',
+  mark:arr[2] || '',
+  unit:arr[3] || '',
+  store:arr[4] || '',
+  position:arr[5] || '',
+  type:arr[6] || '',
+  desc:arr[7] || '',
  }
 }
 class controllerExcel extends Controller {
@@ -21,13 +22,24 @@ class controllerExcel extends Controller {
     const data = await this.app.mysql.select('register') 
     this.ctx.body = data;
   }
-  async uploadExcel() { 
+  async uploadExcelTest() { 
     const file = this.ctx.request.files[0]; 
     const xlsx = require('node-xlsx');  
     let excel = xlsx.parse(file.filepath);
     let firstPage = excel[0].data    
     for(let i = 0;  i <  firstPage.length ;i++){ 
       let s= await this.app.mysql.insert('register',{ username:firstPage[i][1], password:firstPage[i][2]})
+    } 
+    this.ctx.body = { message:'ok'};
+  }
+  async uploadExcel() { 
+    const file = this.ctx.request.files[0]; 
+    const xlsx = require('node-xlsx');  
+    let excel = xlsx.parse(file.filepath);
+    let firstPage = excel[0].data    
+    for(let i = 0;  i <  firstPage.length ;i++){ 
+      let line = col(firstPage[i])
+      let s= await this.app.mysql.insert('register',line)
     } 
     this.ctx.body = { message:'ok'};
   }
